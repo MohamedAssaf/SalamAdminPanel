b53d258117165dd9ebfd35b1e3d9ed9542228c69
yossef-khaled <yossef.k.y333@gmail.com>
2022-02-16 16:26:02 +0200

    export display card

--- src/Resources/Reusables/index.js
+++ src/Resources/Reusables/index.js
@@ -2,3 +2,4 @@ export {default as SalamButton} from './SalamButton/SalamButton';
 export {default as DualAboutUs} from './DualAboutUs/DualAboutUs';
 export {default as AboutUs} from './AboutUs/AboutUs';
 export {default as DynamicFormLabel} from './DynamicFormLabel/DynamicFormLabel';
+export {default as DisplayCard} from './DisplayCard/DisplayCard';
