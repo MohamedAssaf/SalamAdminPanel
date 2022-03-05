8552cdc1c3a855ba6e1c6fb63eb60e7eee727399
yossef-khaled <yossef.k.y333@gmail.com>
2022-03-05 23:28:10 +0200

    export Cards component

--- src/Resources/Reusables/index.js
+++ src/Resources/Reusables/index.js
@@ -3,3 +3,4 @@ export {default as DualAboutUs} from './DualAboutUs/DualAboutUs';
 export {default as AboutUs} from './AboutUs/AboutUs';
 export {default as DynamicFormLabel} from './DynamicFormLabel/DynamicFormLabel';
 export {default as DisplayCard} from './DisplayCard/DisplayCard';
+export {default as Cards} from './Cards/Cards';
