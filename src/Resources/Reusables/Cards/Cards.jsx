a6337abb3bec27c92495feb8346f9eeeb1c88501
yossef-khaled <yossef.k.y333@gmail.com>
2022-04-17 23:47:49 +0200

    Clear non used imports.

--- src/Resources/Reusables/Cards/Cards.jsx
+++ src/Resources/Reusables/Cards/Cards.jsx
@@ -1,10 +1,7 @@
 import DisplayCard from '../DisplayCard/DisplayCard';
-import { Row } from 'react-bootstrap';
 import './Cards.css';
 
 const Cards = function({ items }) {
-
-  console.log(`Type of users ${typeof(items)}`);
   
   return (
     <div className="cards-container">
