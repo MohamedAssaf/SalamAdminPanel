7c252cddb0bf1e7aec0b49a26f980210e8d4efbd
yossef-khaled <yossef.k.y333@gmail.com>
2022-06-02 17:43:46 +0200

    Fix recoil not persistance bug.

--- src/RecoilResources/Atoms.js
+++ src/RecoilResources/Atoms.js
@@ -1,16 +1,22 @@
 import { atom } from "recoil";
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist();
 
 export const websiteDirectionState = atom({
   key: "websiteDirectionState", // unique ID (with respect to other atoms/selectors)
   default: window.sessionStorage.getItem("websiteDirectionState") || "ltr", // default value (aka initial value)
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const websiteLanguageState = atom({
   key: "websiteLanguageState", // unique ID (with respect to other atoms/selectors)
   default: window.sessionStorage.getItem("websiteLanguageState") || "ar", // default value (aka initial value)
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const userState = atom({
   key: "currentUser",
   default: null,
+  effects_UNSTABLE: [persistAtom]
 });
